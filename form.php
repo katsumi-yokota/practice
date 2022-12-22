5511c3d6d78847bbd71fa20b31e7c643a0fcddc9
Katsumi Yokota <katsumi.yokota@shimoning.com>
2022-12-22 16:10:23 +0900

    Modify UI & UX

--- form.php
+++ form.php
@@ -57,12 +57,7 @@ if(isset($_POST['submit']))
 <!DOCTYPE html>
 <html>
 <head>
-         <title>Shimoningにエントリーするしない？</title>  
-         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
-         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" /> 
-         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-         <!-- Bootstrap CSS -->
-         <!-- Bootstrap CSS -->
+         <title>Shimoningにエントリーするしない？</title>   
          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
@@ -76,21 +71,28 @@ if(isset($_POST['submit']))
                     echo $error;
                }
                ?>
-               <label class="p-3">お名前<span class="text-danger"> ※</span></label>
+               <label class="py-4">お名前<span class="text-danger"> ※</span></label>
                <input type="text" name="name"  placeholder="田中太郎" class="form-control" />
-               <label class="p-3">メールアドレス<span class="text-danger"> ※</span></label>
-               <input type="text" name="email"  placeholder="shimoning@gmail.com" class="form-control" />
-               <label class="p-3">性別</label>
-               <input type="text" name="gender" placeholder="男性" class="form-control" />
-               <label class="p-3">ご希望のポジション<span class='text-danger'> ※</span></label>
+               <label class="py-4">メールアドレス<span class="text-danger"> ※</span></label>
+               <!-- <input type="email">を使うことで、バリデーション（エラーチェック）が効く -->
+               <input type="email" name="email" placeholder="shimoning@gmail.com" class="form-control" />
+               <label class="py-4">性別</label>
+               <!-- 性別をプルダウンにする -->
+               <select name="gender" class="container">
+                    <!-- <option selected>で一番初めの文章（この場合は「選択してください」）が表示される -->
+                    <option selected>選択してください</option>
+                    <option value="男性">男性</option>
+                    <option value="女性">女性</option>
+                    <option value="その他">その他</option>
+                    <option value="未回答">未回答</option>
+               </select>
+               <label class="py-4">ご希望のポジション<span class='text-danger'> ※</span></label>
                <input type="text" name="position"  placeholder="SE" class="form-control" />
-               <label class="p-3">前職</label>
+               <label class="py-4">前職</label>
                <input type="text" name="work" placeholder="保険の営業" class="form-control" />
-               <label class="p-3">逆質問（事前にきいておきたいこと）</label>
-               <!-- デザインを整えるために<br>を使うのは本当は良くない -->
-               <br>
+               <label class="py-4">逆質問（事前にきいておきたいこと）</label>
                <textarea class="container" name="question" cols="50" rows="5" placeholder="Shimoningの名前の由来は？"></textarea>
-               <input type="submit" name="submit" value="エントリーする" class="btn btn-success b-3" />
+               <input type="submit" name="submit" value="エントリーする" class="btn btn-success px-5" />
                <?php
                if(!empty($message))
                {
