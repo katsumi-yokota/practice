873fc7da65dccf5c45550545e117aa1825045e91
Katsumi Yokota <katsumi.yokota@shimoning.com>
2022-12-25 16:27:50 +0900

    For MySQL

---
+++ form2.php
@@ -0,0 +1,173 @@
+<!-- //$は変数名の指定に使う -->
+<?php
+//メッセージ、エラーメッセージを表示する
+$message = '';
+$error = '';
+//ドルアンダーバーポスト
+//スーパーグローバル変数はなるべく使わないのが望ましい
+//変数がセットされているか、NULLではないかを確認するにはisset関数を用いる
+if(isset($_POST['submit']))
+{
+     //もし名前欄が空欄なら
+     if(empty($_POST['name']))
+     {
+          $error = "<p class='text-warning text-center display-5'>お名前が入力されていません</p>";
+     }
+     //もしメールアドレス欄が空欄なら
+     else if(empty($_POST['email']))
+     {
+          $error = "<p class='text-warning text-center display-5'>メールアドレスが入力されていません</p>";
+     }
+     //もし希望するポジション欄が空欄なら
+     else if(empty($_POST['position']))
+     {
+          $error = "<p class='text-warning text-center display-5'>ご希望のポジションが入力されていません</p>";
+     }
+     else
+     {
+          //もしentries2.jsonが存在していないならば（！を用いているので「反対」の意味になる）
+          if(!file_exists('entries2.json'))
+          {
+               //touch関数を用いてentries2.jsonをつくる
+               touch('entries2.json');
+          }
+
+          $current_data = file_get_contents('entries2.json'); //ファイルの全内容を文字列に読み込む
+          $array_data = json_decode($current_data, true); //json_decodeでJSON文字列をデコード(復号)する
+          //連想配列
+          //$_POSTはスーパーグローバル変数（定義済み関数）で、HTTP POSTメソッドで送信された値を取得する       
+          $extra = array(
+               'name' => $_POST['name'],
+               'email' => $_POST['email'],
+               'gender' => $_POST['gender'],
+               'position' => $_POST['position'],
+               'work' => $_POST['work'],
+               'question' => $_POST['question'],
+          );
+          $array_data[] = $extra;
+          $final_data[] = json_encode($array_data);
+          //データをファイルに書き込む
+          if(file_put_contents('entries2.json', $final_data)) 
+          {
+               $message = '<p class="text-center text-success display-4">エントリー完了!お疲れ様でした。担当者がご連絡します。</p>';
+          }
+     }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+         <title>[DB用]Shimoningにエントリーするしない？</title>   
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+     <div class="container">
+          <h3 class="text-center">[DB用]Shimoningにエントリーするしない？</h3>
+          <h5 class="text-center"><span class='text-danger'>※</span> は必須</h5>
+          <form method="post">
+               <?php
+               if(!empty($error)) //isset関数とは、引数に指定した変数に値が設定されている、かつ、NULLではない場合にはtrue(正)の値を戻り値とします。 それ以外は、戻り値にfalse(偽)の値を返します。
+               {
+                    echo $error;
+               }
+               ?>
+               <label class="py-4">お名前<span class="text-danger"> ※</span></label>
+               <input type="text" name="name"  placeholder="田中太郎" class="form-control" />
+               <label class="py-4">メールアドレス<span class="text-danger"> ※</span></label>
+               <!-- <input type="email">を使うことで、バリデーション（エラーチェック）が効く -->
+               <input type="email" name="email" placeholder="shimoning@gmail.com" class="form-control" />
+               <label class="py-4">性別</label>
+               <!-- 性別をプルダウンにする -->
+               <select name="gender" class="container">
+                    <!-- <option selected>で一番初めの文章（この場合は「選択してください」）が表示される -->
+                    <option selected>選択してください</option>
+                    <option value="男性">男性</option>
+                    <option value="女性">女性</option>
+                    <option value="その他">その他</option>
+                    <option value="未回答">未回答</option>
+               </select>
+               <label class="py-4">ご希望のポジション<span class='text-danger'> ※</span></label>
+               <input type="text" name="position"  placeholder="SE" class="form-control" />
+               <label class="py-4">前職</label>
+               <input type="text" name="work" placeholder="保険の営業" class="form-control" />
+               <label class="py-4">質問（事前にきいておきたいこと）</label>
+               <textarea class="container" name="question" cols="50" rows="5" placeholder="Shimoningの名前の由来は？"></textarea>
+               <input type="submit" name="submit" value="エントリーする" class="btn btn-success px-5" />
+               <?php
+               if(!empty($message))
+               {
+                    echo $message;
+               }
+               ?>
+          </form>
+     </div>
+</body>
+<footer class='text-center'>(c) shimoning.com</footer>
+</html>
+
+<?php
+// DSN（データソースネーム）
+// utf8:1～3バイトまで対応 utf8mb4:1～4バイトまで対応
+// $dsn = 'mysql:host=localhost;dbname=form-db;charaset=utf8';
+// // ユーザー名。初期設定はrootユーザー（全権限を持つ）が用意されているが、権限を何人かのユーザーに分けるケースが多い
+// $user = 'yamadasan';
+// // パスワード
+// $pass ='1q2w3e4r5t';
+// // オプション...なくてもいい。今回はなし
+
+// try
+// {
+//   $pdo = new PDO
+//   (
+//     $dsn, $user, $pass,
+//     [ 
+//       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//       PDO::ATTR_EMULATE_PREPARES => false,
+//     ]
+//   );
+
+//   $pdo->query
+//   (
+//     'DROP TABLE IF EXISTS entries'
+//   );
+
+//   $pdo->query
+//   (
+//     'CREATE TABLE entries
+//     (
+//       id INT NOT NULL AUTO_INCREMENT,
+//       name text,
+//       email text,
+//       gender text,
+//       position text,
+//       work text,
+//       question text,
+//       PRIMARY KEY (id)
+//     )'
+//   );
+
+//   $pdo->query
+//   (
+//     "INSERT INTO entries (name, email, gender, position, work, question) VALUES
+//       ('Thanks', 15),
+//       ('thanks', 4),
+//       ('bo', 9),
+//       ('boi', 19),
+//       ('boiu', 12),
+//       ('boiup', 17),
+//       ('あ', 17),
+//       ('Arigato', 15)"
+//   );
+
+//   $stmt = $pdo->query('SHOW TABLES');
+//   $entries = $stmt->fetchAll();
+
+// } 
+// catch (PDOException $e)
+// {
+//   echo $e->getMessage();
+//   exit;
+// }   -->
+
+?>
---
+++ show-list2.php
@@ -0,0 +1,85 @@
+<?php
+// 変数fileをつくる
+$file = './entries2.json';
+if(!file_exists("$file"))
+{
+  echo '*ファイルがありません！';
+  return;
+}
+// 変数fileの内容を文字列に取り込む
+$json = file_get_contents($file);
+
+// 変数jsonが空なら「JSONファイルがありません」を出力する
+if(empty($json))
+{
+  echo 'JSONファイルがありません';
+  return;
+}
+
+// 変数jsonが空なら'entries2.json'を作成する
+if(!empty($json))
+{
+  touch('entries2.json');
+}
+
+// 変数jsonをjson_decodeでデコード（復号）
+$entries = json_decode($json, true);
+
+// 変数の情報を開示する
+// var_dump($documents);
+
+if(empty($entries)) 
+{
+  echo'空です！';
+  return;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+  <title>form2.phpに入力された情報をテーブル化する</title> 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  </head>
+
+  <body>
+  <table class="table container">
+  <!-- HTMLのコメントアウト、PHPのコメントアウトの違いに注意 -->
+  <!-- テーブルのヘッダー -->
+  <thead>
+    <tr>
+      <!-- scopeは書かなくてもよい -->
+      <!-- colはcolumn（カラム）のこと -->
+      <th scope="row">#</th>
+      <th scope="col">名前</th>
+      <th scope="col">メールアドレス</th>
+      <th scope="col">性別</th>
+      <th scope="col">希望ポジション</th>
+      <th scope="col">前職</th>
+      <th scope="col">質問</th>
+    </tr>
+  </thead>
+  <!-- テーブルのボディ -->
+<tbody>
+  <!-- 変数entriesから、1個ずつ値（name, email, gender, position, work, question）を取り出す -->
+  <?php foreach ($entries as $i => $entry): ?>
+    <tr>
+      <!-- 可読性を高めるためにも、さっさと計算させる（算数の計算における「足すや掛ける」のように、順番がある）ためにも、こういう場合には（）を使う -->
+      <!-- 変数iはValue。デフォルトで0から始まるので１を足してあげればよい -->
+      <th><?php echo ($i+1); ?></th>
+      <td><?php echo $entry['name']; ?></td>
+      <td><?php echo $entry['email']; ?></td>
+      <td><?php echo $entry['gender']; ?></td>
+      <td><?php echo $entry['position']; ?></td>
+      <td><?php echo $entry['work']; ?></td>
+      <!-- htmlcharactersを用いてHTMLで使われる文字コードを無効化して、単なる文字列として出力する -->
+      <!-- .や,を用いると複数の値をechoできるが、文字コードを無効化できなかったので以下のコードに決定 -->
+      <!-- ENT_QUOTESを指定すると、特殊文字のうちシングルクォーテーションとダブルクォーテーションも変換対象に含めるようになる -->
+      <td><?php echo nl2br(htmlspecialchars($entry['question'], ENT_QUOTES, 'UTF-8')); ?></td>
+    </tr>
+  <!-- HTMLの中にPHPを書く時に{}があるとどのブロックを閉じるのかがわかりにくいため、endforceが好んで用いられる -->
+  <?php endforeach; ?>
+  </tbody>
+  </table>
+  </body>
+</html>
