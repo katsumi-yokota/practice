d991e5427507acfb46a99902bb27f5cd1eb19b59
Katsumi Yokota <katsumi.yokota@shimoning.com>
2023-02-10 19:02:31 +0900

    Add status code

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$_SESSION = array();
 session_destroy();
+http_response_code(200);
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +10,7 @@ session_destroy();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>ログアウト</title>
 </head>
 <body>
   <p>ログアウトしました。</p>
