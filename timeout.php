f5d5f2e7fa428c14c5e79a199ac1d9bb644683ad
Katsumi Yokota <katsumi.yokota@shimoning.com>
2023-02-11 17:42:20 +0900

    Create timeout file for automatic logout function

---
+++ timeout.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>Timeout</title>
+</head>
+<body>
+  <div class="container text-center">
+    <h1 class="my-5">タイムアウト</h1>
+    <div class="bg-light my-5 py-5">
+     <p class="">一定時間操作がなかったためログアウトしました。</p>
+     <p class="">エントリー一覧の利用を続ける場合は、「ログインする」ボタンをクリックして遷移先でログインしてください。</p>
+    </div>
+    <a href="login.php" class="btn btn-success btn-lg px-5">ログインする</a>
+  </div>
+</body>
+</html>
